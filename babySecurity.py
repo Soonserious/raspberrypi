10ef41bd07a783fa5e1dd6aa0effb3de0861cfd3
Soonserious <eotlr9404@naver.com>
2019-08-21 07:35:09 +0100

    Add files via upload

--- babySecurity.py
+++ babySecurity.py
@@ -6,22 +6,30 @@ import subprocess
 import argparse
 import secrets
 import logging
+import json
 
 
 logging.basicConfig(filename="./status.log",level=logging.DEBUG)
 
 def createData():
-    return {"authentication": "ekscn0805"}
+    key = ""
+    with open("/home/pi/authentication.txt","r") as f:
+        for line in f:
+            key = line.rstrip("\n")
+    print(key)
+    return {"authentication": key}
 
 
 def serverRequest(serverURL, data, upload):
     client = requests.session()
     serverURL = "http://192.168.0.68:8000/rasberrypy/" + serverURL
+    client.get(serverURL)
     csrfToken = client.cookies["csrftoken"]
     headers = {"X-CSRFToken": csrfToken}
-    res = client.post(serverURL, files=upload, data=data, headers=headers).json()
+    res = client.post(serverURL, files=upload, data=data, headers=headers)
+    response = res.json()
     client.close()
-    return res
+    return response 
 
 
 def createBabyPicture():
@@ -32,7 +40,7 @@ def createBabyPicture():
     files = open(imagePath, "rb")
     upload = {"image": files}
     data = createData()
-    return serverRequest("imageUpload", data=data, upload=upload)
+    return json(serverRequest("imageUpload", data=data, upload=upload))
 
 
 def createBabyTemperature():
@@ -49,28 +57,48 @@ def createBabyTemperature():
 
 def createKey():
     key = secrets.token_hex(16)
-    data = {"authentication" : key}
+    data = {"offerId" : "sktds92",
+            "authentication" : key}
     result = serverRequest("createProduction", data= data,upload=None)
-    if result["status"]:
+    if not result["status"]:
         logging.debug("not Authentication")
     else:
-        with open("/authentication.txt","w") as f:
+        with open("/home/pi/authentication.txt","w") as f:
             f.write(result["authentication"])
             f.close()
-        os.chmod("/authentication.txt",744)
+        os.chmod("/home/pi/authentication.txt",744)
 
 def createUser():
     s = input("당신의 ID를 입력해주세요")
+    data =  createData()
+    data["userId"]=s
+    jsonResponse = serverRequest("productAuth",data=data,upload=None)
+    if jsonResponse["status"]:
+        with open("/home/pi/status.txt","w") as f:
+            f.write(1)
+        os.chmod("/home/pi/status.txt",744)
+
+
+def operation():
+    status = ""
+    with open("/home/pi/status.txt","r") as f:
+        for line in f:
+            status = line.rstrip("\n")
+    if status == "1":
+        while(1):
+             createBabtPicture()
+             createBabyTemperature()
 
 if __name__ == "__main__":
     logging.basicConfig(filename="./status.log", level=logging.DEBUG)
     parser = argparse.ArgumentParser("")
     parser.add_argument("--operationMode", required=True)
     args = parser.parse_args()
-    if args == 1:
+    if args.operationMode == "1":
         createKey()
-    elif args == 2:
+    elif args.operationMode == "2":
         createUser()
     else:
+     #   operation()
         createBabyPicture()
         createBabyTemperature()
